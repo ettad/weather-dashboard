aee61194cb64cc1981a5ae8db9f160d3afa812e2
Tadesse Tilahun <ettad2@gmail.com>
2022-10-16 23:36:23 -0600

    setting up API all

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var todaysDate = moment().format('ddd DD YYYY');
 $(".day").html(todaysDate);
 
 
- 
+ // sets the five days date for the daily forcast
 function date () {
     $(".days").each(function () {
         for (i=1; i<6; i++) {
@@ -14,3 +14,25 @@ function date () {
 }
 //calls the date function
 date();
+
+
+let weather = {
+    apiKey: "API KEY GOES HERE",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+
+}
